ca504f6617c43a76d566a88a6c9156bd09aaedb1
veith <veithz@gmail.com>
2020-10-16 09:20:05 +0200

    spectools v1.11.2 Ordering of required, readonly

--- spectools.rb
+++ spectools.rb
@@ -4,8 +4,8 @@
 class Spectools < Formula
   desc "Furo spectools utility toolkit around the furo specs"
   homepage "https://github.com/theNorstroem/spectools/blob/master/README.md"
-  url "https://github.com/theNorstroem/spectools/archive/v1.11.1.tar.gz"
-  sha256 "5f9cde1c9243f8e199abf6951c58abb40cbbba8a8cbebe449c7d44f0c6f63809"
+  url "https://github.com/theNorstroem/spectools/archive/v1.11.2.tar.gz"
+  sha256 "a8eb20405eec271553e98b951f3b4e4b0e6a4c698a9a642711d4ed68375deb9e"
   license "MIT"
   depends_on "go"
   def install
