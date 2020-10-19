dd450887a36603646814959225079b2cc3d6970e
veith <veithz@gmail.com>
2020-10-19 10:30:52 +0200

    spectools v1.13.1 fix: version number

--- spectools.rb
+++ spectools.rb
@@ -4,8 +4,8 @@
 class Spectools < Formula
   desc "Furo spectools utility toolkit around the furo specs"
   homepage "https://github.com/theNorstroem/spectools/blob/master/README.md"
-  url "https://github.com/theNorstroem/spectools/archive/v1.13.0.tar.gz"
-  sha256 "8d4565f21a4c9b3f374408a04de296b0fe1fe113ccf1467dda1efe8201618bbc"
+  url "https://github.com/theNorstroem/spectools/archive/v1.13.1.tar.gz"
+  sha256 "4dafd7f919d8127b092cb426142be5ccfd66c910b3186f440b8ae28fdecf39c2"
   license "MIT"
   depends_on "go"
   def install
