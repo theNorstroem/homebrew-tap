ec1ef24a2ec21f069919f77ef792545553faa354
veith <veithz@gmail.com>
2020-11-02 16:16:30 +0100

    spectools v1.18.0 feat: dist file management

--- spectools.rb
+++ spectools.rb
@@ -4,8 +4,8 @@
 class Spectools < Formula
   desc "Furo spectools utility toolkit around the furo specs"
   homepage "https://github.com/theNorstroem/spectools/blob/master/README.md"
-  url "https://github.com/theNorstroem/spectools/archive/v1.17.0.tar.gz"
-  sha256 "4641fbc8adfe7fc15c5429157e9e128391b27facaac54db93233090d06e1102f"
+  url "https://github.com/theNorstroem/spectools/archive/v1.18.0.tar.gz"
+  sha256 "409c32fc18c637229903f66d699e9deb6672387d852e0a5a1180a978eadc5b52"
   license "MIT"
   depends_on "go"
   def install
