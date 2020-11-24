7be5895355c4b4cea4014762702d787487210852
veith <veithz@gmail.com>
2020-11-24 12:21:12 +0100

    spectools v1.21.2 Flag for overwriting the options setted in specs added. muSpec2Spec --overwrite-spec-options

--- spectools.rb
+++ spectools.rb
@@ -4,8 +4,8 @@
 class Spectools < Formula
   desc "Furo spectools utility toolkit around the furo specs"
   homepage "https://github.com/theNorstroem/spectools/blob/master/README.md"
-  url "https://github.com/theNorstroem/spectools/archive/v1.21.1.tar.gz"
-  sha256 "a7a4cf7a3e97b1523133fec2947977acbe906b9e60ded0168c72eb34a3307602"
+  url "https://github.com/theNorstroem/spectools/archive/v1.21.2.tar.gz"
+  sha256 "593b108bbc68623e151ecd92413c9d94bf485e67dfe99e0485029765f3a337f1"
   license "MIT"
   depends_on "go"
   def install
